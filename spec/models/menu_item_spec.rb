0f6587b5fec388b881283b4c31704ca8008ab28d
Austin Aldrich <aualdrich@gmail.com>
2026-06-27 08:43:59 -0400

    Add menu items validations

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -3,6 +3,14 @@ require "rails_helper"
 RSpec.describe MenuItem, type: :model do
   fixtures :menu_items
 
+  subject(:menu_item) do
+    described_class.new(
+      name: "Test Burger",
+      price_cents: 100,
+      prep_seconds: 60
+    )
+  end
+
   describe ".order(:id)" do
     it "returns the seeded menu items in the expected order" do
       expect(
@@ -17,5 +25,14 @@ RSpec.describe MenuItem, type: :model do
         ]
       )
     end
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_presence_of(:price_cents) }
+    it { is_expected.to validate_numericality_of(:price_cents).is_greater_than(0) }
+    it { is_expected.to validate_presence_of(:prep_seconds) }
+    it { is_expected.to validate_numericality_of(:prep_seconds).is_greater_than(0) }
   end
 end
